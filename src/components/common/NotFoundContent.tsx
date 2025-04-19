54f930dcdd9f2595ae88bfeb9d3dc6adb909c37e
Vinoth-PSD <vinoth@psdigitise.com>
2025-04-19 16:08:08 +0530

    Update NotFoundContent.tsx

--- src/components/common/NotFoundContent.tsx
+++ src/components/common/NotFoundContent.tsx
@@ -6,7 +6,7 @@ export const NotFoundContent = () => {
             <div className="container mx-auto">
                 <div className="text-center space-y-8">
                     <h5 className="text-[24px] font-bold text-black">Oops! Nothing Here</h5>
-                    <p className="text-base font-normal text-black">No salon nearby at the moment, but we’re growing! Come back in a month—we might be closer than you think.</p>
+                    <p className="text-base font-normal text-black">No salon/freelancer nearby at the moment, but we’re growing! Come back in a month—we might be closer than you think.</p>
                     <div>
                     <img src={notFoundImage} alt="notFoundimage" className="w-40 mx-auto" />
                     </div>
